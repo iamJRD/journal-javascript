efbd9038ad3edf7c3b7c90980a4c2c86422b794d
jared beckler and michael lor <jared.beckler@gmail.com>
2016-03-14 10:24:26 -0700

    add moment to date input

--- js/cli.js
+++ js/cli.js
@@ -1,6 +1,8 @@
 var wordCount = require('./wordCount.js').wordCount;
+var formattedDate = require('./formattedDate.js').formattedDate;
 var prompt = require('prompt');
 prompt.start();
+
 
 prompt.get('entry_body', function(err, test) {
 var test = wordCount(test.entry_body);
