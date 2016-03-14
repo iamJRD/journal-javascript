16983f268ca297d243f09b9d132bd435c1178732
jared beckler and michael lor <michael.vang.1985@gmail.com>
2016-03-14 08:56:24 -0700

    add prompt package via npm

--- js/cli.js
+++ js/cli.js
@@ -1,4 +1,5 @@
 var wordCount = require('./wordCount.js').wordCount;
 
+var entry_body = prompt('enter your journal entry');
 var test = wordCount("something random is happening");
 console.log(test);
